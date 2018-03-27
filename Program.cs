10008cfa7fd61763143ec1ac085926b8fc025aa7
HarunYetgindag <37370113+HarunYetgindag@users.noreply.github.com>
2018-03-27 14:03:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Composite root = new Composite("Root");
+            root.Add(new Leaf("Leaf A"));
+            root.Add(new Leaf("Leaf B"));
+
+            Composite comp = new Composite("Composite X");
+            comp.Add(new Leaf("Leaf XA"));
+            comp.Add(new Leaf("Leaf XB"));
+            root.Add(comp);
+            root.Add(new Leaf("Leaf C"));
+
+            Leaf leaf = new Leaf("Leaf D");
+            root.Add(leaf);
+
+            root.Display(1);
+
+            Console.Read();
+        }
+    }
+
+    abstract class Component
+    {
+        protected string _name;
+
+        public Component(string name)
+        {
+            _name = name;
+        }
+
+        public abstract void Add(Component c);
+        public abstract void Remove(Component c);
+        public abstract void Display(int depth);
+
+    }
+
+    class Composite : Component
+    {
+        private List<Component> _children = new List<Component>();
+        public Composite(string name) : base(name)
+        {
+        }
+
+        public override void Add(Component c)
+        {
+            _children.Add(c);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + _name);
+            foreach (Component component in _children)
+            {
+                component.Display(depth + 2);
+            }
+        }
+
+        public override void Remove(Component c)
+        {
+            _children.Remove(c);
+        }
+    }
+
+    class Leaf : Component
+    {
+        public Leaf(string name) : base(name)
+        {
+        }
+
+        public override void Add(Component c)
+        {
+            Console.WriteLine("Eklenemez...");
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + _name);
+        }
+
+        public override void Remove(Component c)
+        {
+            Console.WriteLine("Silinemez...");
+        }
+    }
+}
